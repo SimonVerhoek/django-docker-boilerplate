a06c1c15aa83c3f0e5cc7a9e0914ae23441e86df
Simon Verhoek <simon@kaartencarrousel.nl>
2019-05-15 14:40:52 +0200

    changed yddt to default_app to create a boilerplate repo

--- app/default_app/wsgi.py
+++ app/default_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for yddt project.
+WSGI config for default_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yddt.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'default_app.settings')
 
 application = get_wsgi_application()
